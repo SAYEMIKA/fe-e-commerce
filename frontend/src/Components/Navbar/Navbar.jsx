b3758081f75e406b0671ef565fd52ac8be40fe45
SAYEMIKA <115859922+SAYEMIKAA@users.noreply.github.com>
2025-11-04 14:02:44 +0700

    ubah nama header dari ALBANI ke MIK.

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ export const Navbar = () => {
     <div className='navbar'>
         <div className="nav-logo">
             <img src={logo} alt="" />
-            <p>ALBANI</p>
+            <p>MIK.</p>
         </div>
         <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
         <ul ref={menuRef} className="nav-menu">
